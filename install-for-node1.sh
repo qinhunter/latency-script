787a50d984158c74f0a0822cd9cf5ac65e2b2adc
qinhunter <254822498@qq.com>
2016-10-29 20:30:58 +0800

    Update install-for-node1.sh

--- install-for-node1.sh
+++ install-for-node1.sh
@@ -8,6 +8,7 @@ sudo apt-get install iperf
 
 #install sockperf 
 
+sudo apt-get install perl automake autoconf m4 libtool gcc g++
 git clone https://github.com/Mellanox/sockperf.git
 cd sockperf
 ./autogen.sh 
